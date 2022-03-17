50d29a0d97994fb6cc0b9bc53a5ad50d92d36c5c
Jhon Castiblanco <jhoncasti2003@gmail.com>
2022-03-16 22:57:42 -0500

    Array Destructuring
    [ADD]: example of work with destructuring in array.

---
+++ js/arrayDestructuring.js
@@ -0,0 +1,8 @@
+
+// creacion de array
+const tecnologias = ['React','Java','NodeJs','Golang']
+
+// La destructuracion de array trabaja con posiciones
+const [ , , , lenguaje] = tecnologias;
+
+console.log(lenguaje)
